67b7f7d466576fdc1ea12156ca359cfd71fb7f0a
longziyun712 <msm20021031@gmail.com>
2025-06-23 11:07:53 +0800

    Delete my-folder/inspectionProfiles directory

--- my-folder/inspectionProfiles/─г─т┐╝ ╫ў╥╡C/06364 ┼г╡─╤б╛┘.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict #为缺失键自动创建
-n,k=map(int,input().split())
-dic=defaultdict()
-for i in range(1,n+1):
-    dic[i]=list(map(int,input().split()))
-d=dict(sorted(dic.items(),key=lambda x:x[1][0],reverse=True)[:k]) #sorted是列表
-n=sorted(d.items(),key=lambda x:x[1][1],reverse=True)[0][0]
-print(n)
